e3f0fb415331b98a83f793365179662c75009677
Edwin Alvarado <edwinalvaradoster@gmail.com>
2017-11-02 16:55:23 -0400

    p2pActivity updates and SplitPayment

---
+++ app/src/main/java/com/btm/pagodirecto/activities/SplitPaymentActivity.java
@@ -0,0 +1,15 @@
+package com.btm.pagodirecto.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.btm.pagodirecto.R;
+
+public class SplitPaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_split_payment);
+    }
+}
